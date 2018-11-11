4cbdcef9b9fa6980b3922614edf6096c872fba32
Matt Andrzejczuk <matt@jawn.it>
2018-11-10 19:37:13 -0500

    first code

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='PyInk',
+      version='0.1',
+      description='Colored output for your console.',
+      long_description='Fast and easy way to print colored output to your console.',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Console :: Shell',
+      ],
+      keywords='console shell color print output',
+      url='',
+      author='Matt Andrzejczuk',
+      author_email='cinicraft@me.com',
+      license='MIT',
+      packages=['pyink'],
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False)
